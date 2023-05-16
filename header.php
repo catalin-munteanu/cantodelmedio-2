995dd26da64ab62a6f987c0f6408f00200db2456
Catalín Munteanu <catalinapampin@gmail.com>
2023-05-15 22:26:38 -0300

    Commit inicial

---
+++ header.php
@@ -0,0 +1,19 @@
+<header>
+        <ul id="menuRedesHeader">
+            <li class="iconRedesHeader"><a href="https://www.instagram.com/elcantodelmedio/" target="_blank"><img src="..\wp-content\themes\cantodelmedio\img\mobile\logo-final-ig.png" class="imgRedesHeader" id="logoInstagramHeader"></a></li>
+            <li class="iconRedesHeader"><a href="https://www.youtube.com/channel/UCLtgVMuTefGp63pZiPophPA" target="_blank"><img src="..\wp-content\themes\cantodelmedio\img\mobile\logo-final-yt.png" class="imgRedesHeader" id="logoYoutubeHeader"></a></li>
+            <li class="iconRedesHeader"><a href="https://wa.me/5491136148307" target="_blank"><img src="..\wp-content\themes\cantodelmedio\img\mobile\logo-final-wa.png" class="imgRedesHeader" id="logoWhatsAppHeader"></a></li>
+        </ul>
+
+        <button id="burger-menu-toggler">
+            <img src="..\wp-content\themes\cantodelmedio\img\mobile\burger-menu.png" id="open-mobile-menu"></img>
+            <img src="..\wp-content\themes\cantodelmedio\img\mobile\close-burger-menu.png" id="close-mobile-menu"></img>
+        </button>
+
+        <ul id="mainMenu">
+            <li class="itemsNav"><a href="..\index.php\#formacionIntegral">Formación integral</a></li>
+            <li class="itemsNav"><a href="..\index.php\#consultoriaVoz">Consultorías</a></li>
+            <li class="itemsNav"><a href="..\index.php\#asesoriaVoz">Asesorías</a></li>
+            <li class="itemsNav"><a href="..\home">Blog</a></li>
+        </ul>
+    </header>
---
+++ page-programa-curso.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../wp-content/themes/cantodelmedio/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;600;700&family=Cabin:wght@400;700&family=Quicksand:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;700&family=Quicksand:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <script src="..\wp-content\themes\cantodelmedio\js\main.js"></script>
+    <title>El canto del medio | Formación integral en Canto Consciente y Visionario | Programa completo</title>
+</head>
+
+<body>
+
+    <?php require_once 'header.php'; ?> 
+
+    <main>
+
+        <section id="sectProgramaCompleto">
+            <h1 id="progCompleto">Formación integral en Canto Consciente y Visionario</h1>
+            <h4 id="progCompletoSubtitulo">Programa completo</h4>
+            <article id="containerTablaProg">
+                <img src="..\wp-content\themes\cantodelmedio\img\mobile\cuadro-programa-completo.png" class="tabla-prog-330">
+                <img src="..\wp-content\themes\cantodelmedio\img\mobile\cuadro-programa-completo-768.png" class="tabla-prog-768">
+                <!--<figure class="imgFondo">
+                <img src="..\wp-content\themes\cantodelmedio\img\mobile\fondo-negro.svg" alt="" srcset="">
+            </figure>
+            <table id="tablaProgCompleto">
+                <tr>
+                    <th>Nivel 1</th>
+                    <th></th>
+                    <th>Nivel 2</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td class="celdaConCirculo">
+                    <img class="circuloEnCelda" src="..\wp-content\themes\cantodelmedio\img\mobile\trazo-circ-4.svg" alt="" srcset="">    
+                    <div class="textoEnCeldaConCirculo">Profundización en<br>canto consciente<br>(cuatrimestre)</div></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><img class="trazosFlecha" src="..\wp-content\themes\cantodelmedio\img\mobile\flecha-1.svg"></td>
+                    <td></td>
+                    <td><img class="trazosFlecha" src="..\wp-content\themes\cantodelmedio\img\mobile\flecha-2.svg"></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="celdaConCirculo">
+                    <img class="circuloEnCelda" src="..\wp-content\themes\cantodelmedio\img\mobile\trazo-circ-3.svg" alt="" srcset="">
+                    <div class="textoEnCeldaConCirculo">Iniciación al<br>canto consciente<br>(cuatrimestre)</div></td>
+                    <td><img class="trazosFlecha" src="..\wp-content\themes\cantodelmedio\img\mobile\flecha-derecha.svg"></td>
+                    <td class="celdaConCirculo">
+                    <img class="circuloEnCelda" src="..\wp-content\themes\cantodelmedio\img\mobile\trazo-circ-5.svg" alt="" srcset="">
+                    <div class="textoEnCeldaConCirculo">Profundización en<br>canto visionario<br>(cuatrimestre)</div></td>
+                    <td><img class="trazosFlecha" src="..\wp-content\themes\cantodelmedio\img\mobile\flecha-derecha.svg"></td>
+                    <td class="celdaConCirculo">
+                    <div class="textoEnCeldaConCirculo">Supervisiones</div></td>
+                </tr>
+            </table>-->
+            </article>
+
+        <article id="descr-programa-formacion">
+            <p>El programa de Formación está diseñado para que realices un recorrido de aprendizaje que te permita adquirir los conocimientos teóricos y las habilidades prácticas esenciales para que el contacto con tu Voz sea saludable, orgánico, expresivo, creativo y musicalmente eficiente. Nuestro enfoque se caracteriza por estudiar a la Voz de una forma integral, es decir, contemplando la intrínseca relación de la creación de sonido humano con los atravesamientos emocionales alojados en las memorias del cuerpo, con las creencias personales y colectivas que condicionan o potencian nuestra expresión y también, con el desarrollo y la construcción de la identidad personal en relación a un contexto, cultura o subcultura.</p>
+            <p>La Formación en <strong class="textoNegrita">Canto Consciente</strong> está dirigida a quienes deseen aplicar estos conocimientos en entornos artísticos y creativos. La Formación en <strong class="textoNegrita">Canto Visionario</strong>, a quienes quieran aplicarlos en entornos terapéuticos y de sanación.</p>
+            <p>Ambos enfoques se completan con 2 niveles de estudio. Una vez realizados, contás con la posibilidad de anotarte en las reuniones de <strong class="textoNegrita">Supervisión</strong>, en donde resolveremos dudas y compartiremos hallazgos o reflexiones surgidas de las experiencias prácticas de los estudiantes. </p>  
+        </article>    
+        </section>
+
+        <section id="containerTodasDescripciones">
+        
+            <figure class="imgFondo">
+                <img src="..\wp-content\themes\cantodelmedio\img\mobile\fondo-amarillo-programa.svg" alt="" srcset="">
+            </figure>
+            
+        <div id="sectConscienteNivelUno">
+            <h2>Iniciación al canto consciente<strong> (*)</strong></h2>
+            <h3>Nivel 1</h3>
+
+            <article id="descripcionConscienteNivelUno">
+                <p>El Nivel 1 de la Formación Integral en <strong class="textoNegrita">Canto Consciente</strong> es un curso teórico - práctico cuatrimestral y de modalidad grupal orientado al estudio de las bases funcionales esenciales de la emisión neurofisiológica. Estas bases son las que sustentan el desarrollo de un canto orgánico, saludable, expresivo y musicalmente eficiente.</p></article>
+                <p>(*) El curso de iniciación al Canto consciente corresponde al nivel 1 de formación obligatoria para acceder a los demás cursos facilitados por la escuela.</p>
+            </article>
+
+            <article id="contenidosConscienteNivelUno">
+                <h3>Contenidos del curso</h3>
+                <p>- Estimulación propioceptiva de la relación entre postura, respiración y emisión</p>
+                <p>- Reconocimiento y diferenciación de hábitos compensatorios y funcionales en la relación entre postura, respiración y emisión</p>
+                <p>- Concientización de apoyos, ejes y alineación de los centros respiratorio, fonatorio y resonador</p>
+                <p>- Reconocimiento y elongación de la musculatura laríngea, respiratoria y expresiva</p>
+                <p>- Registro y entrenamiento de la ciclicidad respiración-emisión</p>
+                <p>- Reconocimiento y activación de las válvulas de cierre y apertura laríngeas y pélvicas</p>
+                <p>- Registro de las vías respiratorias superiores y entrenamiento de la base articulatoria</p>
+                <p>- Reconocimiento y diferenciación de los registros vocales grave, medio y agudo</p>
+                <p>- Exploración de cavidades vibro-acústicas de resonancia</p>
+                <p>- Reconocimiento y diferenciación de creencias condicionantes y estimulantes respecto a la voz y al canto</p>
+            </article>
+        </div>
+
+        <img src="..\wp-content\themes\cantodelmedio\img\mobile\tipito-black.png" class="elementoSeccionUno" id="separadorPrograma">
+
+        <div id="sectConscienteNivelDos">
+
+            <article id="descripcionConscienteNivelDos">
+                <h2>Profundización en canto consciente (cuatrimestre)</h2>
+                <h3>Nivel 2</h3>
+
+                <p>El Nivel 2 de la Formación Integral en <strong class="textoNegrita">Canto Consciente</strong> es un curso teórico - práctico cuatrimestral y de modalidad grupal orientado al fortalecimiento de las bases funcionales de la emisión neurofisiológica estudiadas en el nivel 1. También, se propone acompañar la creación de hábitos de entrenamiento que potencien la integración y aplicación de lo aprendido en la vida artística y creativa.</p>
+            </article>
+            
+            <article id="contenidosConscienteNivelDos">
+                <h3>Contenidos del curso</h3>
+                <p>- Desprogramación de hábitos compensatorios</p>
+                <p>- Fortalecimiento de hábitos funcionales</p>
+                <p>- Tonificación y flexibilización de la musculatura laríngea, respiratoria y expresiva</p>
+                <p>- Fortalecimiento de la ciclicidad respiración-emisión</p>
+                <p>- Fortalecimiento de la base articulatoria</p>
+                <p>- Entrenamiento de los registros grave, medio y agudo</p>
+                <p>- Utilización de cavidades vibro-acústicas de resonancia</p>
+                <p>- Flexibilización de la activación de las válvulas de cierre y apertura laríngeas y pélvicas</p>
+            </article>
+        </div>
+
+        <img src="..\wp-content\themes\cantodelmedio\img\mobile\tipito-black.png" class="elementoSeccion1" id="separadorProgramaDos">
+
+        <div id="descripcionVisionarioNivelDos">
+            <article id="contenidosVisionarioNivelDos">
+                <h2>Profundización en canto visionario (cuatrimestre)</h2>
+                <h3>Nivel 2</h3>
+                <p>El Nivel 2 de la Formación Integral en <strong class="textoNegrita">Canto Visionario</strong> es un curso teórico-práctico cuatrimestral y de modalidad grupal orientado al estudio de la voz como medio de canalización akáshica o cuántica. Se propone entrenar estados de presencia expandida por medio de la escucha holística para favorecer su aplicación en ámbitos terapéuticos y de sanación.</p>
+            </article>
+
+            <article id="contenidosVisionario">
+                <h3>Contenidos del curso</h3>
+                <p>Próximamente</p>
+            </article>
+        </div>
+
+        <img src="..\wp-content\themes\cantodelmedio\img\mobile\tipito-black.png" class="elementoSeccion1" id="separadorProgramaTres">
+
+        <div id="descripcionSupervisiones">
+            <h2>Supervisiones</h2>
+            <p>Una vez completado el recorrido de aprendizaje en Canto Consciente o en Canto Visionario, contás con la posibilidad de anotarte en las reuniones de supervisión (1 cada 2 meses) en donde resolveremos dudas y compartiremos hallazgos o reflexiones surgidas de las experiencias prácticas de los estudiantes.</p>
+</div>
+</section>
+    </main>
+
+    <footer>
+                <p>Diseño y desarrollo del sitio: <strong class="textoNegrita">Catalina Pampin</strong> ◆ Diseño de logos e imagen de fondo: <strong class="textoNegrita">Mariana Szulman</strong></p>
+    </footer>
+</body>
+</html>
